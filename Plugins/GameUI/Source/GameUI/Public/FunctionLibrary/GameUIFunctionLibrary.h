f5b6a11c63dd669d839bb45b65bdd66b87996784
Z1145IKUN <2065968420@qq.com>
2025-12-18 16:42:09 +0800

    add UGameUIFunctionLibrary file

---
+++ Plugins/GameUI/Source/GameUI/Public/FunctionLibrary/GameUIFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GameUIFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEUI_API UGameUIFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
