0b42f47f899fceef8034ffb634e6836095e22312
Z1145IKUN <2065968420@qq.com>
2025-12-18 15:03:52 +0800

    Create GameUISubsystem and RegisterPrimaryLayout function

--- Plugins/GameUI/Source/GameUI/Private/Subsystem/GameUISubsystem.cpp
+++ Plugins/GameUI/Source/GameUI/Private/Subsystem/GameUISubsystem.cpp
@@ -3,3 +3,33 @@
 
 #include "Subsystem/GameUISubsystem.h"
 
+UGameUISubsystem* UGameUISubsystem::Get(const UObject* WorldContextObject)
+{
+	if (GEngine)
+	{
+		UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::Assert);
+
+		return UGameInstance::GetSubsystem<UGameUISubsystem>(World->GetGameInstance());
+	}
+	return nullptr;
+}
+
+bool UGameUISubsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+	if (!CastChecked<UGameInstance>(Outer)->IsDedicatedServerInstance())
+	{
+		TArray<UClass*> FoundClasses;
+		// 获取当前类的所有派生类
+		GetDerivedClasses(GetClass(), FoundClasses);
+
+		return FoundClasses.IsEmpty();
+	}
+
+	return false;
+}
+
+void UGameUISubsystem::RegisterPrimaryLayout(UWidget_PrimaryLayout* InPrimaryLayout)
+{
+	check(InPrimaryLayout);
+	CreatedPrimaryLayout = InPrimaryLayout;
+}
--- Plugins/GameUI/Source/GameUI/Public/Subsystem/GameUISubsystem.h
+++ Plugins/GameUI/Source/GameUI/Public/Subsystem/GameUISubsystem.h
@@ -6,6 +6,8 @@
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "GameUISubsystem.generated.h"
 
+
+class UWidget_PrimaryLayout;
 /**
  * 
  */
@@ -13,5 +15,18 @@ UCLASS()
 class GAMEUI_API UGameUISubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
-	
+
+public:
+	static UGameUISubsystem* Get(const UObject* WorldContextObject);
+
+	//~ Begin USubsystem
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+	//~ End USubsystem
+
+	UFUNCTION(BlueprintCallable, Category="GameUI|GameUISubsystem")
+	void RegisterPrimaryLayout(UWidget_PrimaryLayout* InPrimaryLayout);
+
+private:
+	UPROPERTY(Transient)
+	TObjectPtr<UWidget_PrimaryLayout> CreatedPrimaryLayout;
 };
