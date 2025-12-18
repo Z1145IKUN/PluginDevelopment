8ce42864abedf8728b469337e0dd5cc2771c65d4
Z1145IKUN <2065968420@qq.com>
2025-12-18 13:41:38 +0800

    Add game mode base and player controller in game folder

---
+++ Plugins/GameUI/Source/GameUI/Public/Game/GameUIPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GameUIPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEUI_API AGameUIPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
