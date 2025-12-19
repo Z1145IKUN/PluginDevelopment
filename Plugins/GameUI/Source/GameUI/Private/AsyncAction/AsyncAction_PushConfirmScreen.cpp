6224cbc5ee7440500f2be899692c4cd691e0d155
Z1145IKUN <2065968420@qq.com>
2025-12-19 13:32:42 +0800

    Complete the ConfirmScreen, including its style, and exit the game

--- Plugins/GameUI/Source/GameUI/Private/AsyncAction/AsyncAction_PushConfirmScreen.cpp
+++ Plugins/GameUI/Source/GameUI/Private/AsyncAction/AsyncAction_PushConfirmScreen.cpp
@@ -3,3 +3,42 @@
 
 #include "AsyncAction/AsyncAction_PushConfirmScreen.h"
 
+#include "Subsystem/GameUISubsystem.h"
+
+UAsyncAction_PushConfirmScreen* UAsyncAction_PushConfirmScreen::PushConfirmScreen(
+	const UObject* WorldContextObject,
+	EConfirmScreenType InConfirmScreenType, FText InScreenTitle, FText InScreenMessage)
+{
+	if (GEngine)
+	{
+		if (UWorld* World = GEngine->
+			GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+		{
+			UAsyncAction_PushConfirmScreen* Node = NewObject<UAsyncAction_PushConfirmScreen>();
+			Node->CachedWorld = World;
+			Node->CachedConfirmScreenType = InConfirmScreenType;
+			Node->CachedScreenTitle = InScreenTitle;
+			Node->CachedScreenMessage = InScreenMessage;
+
+			Node->RegisterWithGameInstance(World);
+			return Node;
+		}
+	}
+	return nullptr;
+}
+
+void UAsyncAction_PushConfirmScreen::Activate()
+{
+	Super::Activate();
+
+	UGameUISubsystem::Get(CachedWorld.Get())->PushConfirmScreenToModalStackAsync(
+		CachedConfirmScreenType,
+		CachedScreenTitle,
+		CachedScreenMessage,
+		[this](EConfirmScreenButtonType ClickedButtonType)
+		{
+			OnConfirmScreenButtonClicked.Broadcast(ClickedButtonType);
+			SetReadyToDestroy();
+		}
+	);
+}
