0772cbe92ebe5310cd458bb16b40e5e9b684d509
Z1145IKUN <2065968420@qq.com>
2025-12-19 16:58:21 +0800

    Create Widget_TabListWidgetBase file

---
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Options/Widget_TabListWidgetBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonTabListWidgetBase.h"
+#include "Widget_TabListWidgetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, meta=(DisableNativeTick))
+class GAMEUI_API UWidget_TabListWidgetBase : public UCommonTabListWidgetBase
+{
+	GENERATED_BODY()
+	
+};
