e97082de5fd9663d9d585a305f2c30381a06a8ee
Z1145IKUN <2065968420@qq.com>
2025-12-19 16:32:34 +0800

    Custom bind action: reset action

--- Plugins/GameUI/Source/GameUI/Private/Widget/Options/Widget_OptionScreen.cpp
+++ Plugins/GameUI/Source/GameUI/Private/Widget/Options/Widget_OptionScreen.cpp
@@ -3,3 +3,44 @@
 
 #include "Widget/Options/Widget_OptionScreen.h"
 
+#include "ICommonInputModule.h"
+#include "Input/CommonUIInputTypes.h"
+
+void UWidget_OptionScreen::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	if (!ResetAction.IsNull())
+	{
+		ResetAction_Handle = RegisterUIActionBinding(
+			FBindUIActionArgs(
+				ResetAction,
+				true,
+				FSimpleDelegate::CreateUObject(
+					this,
+					&ThisClass::OnResetActionTriggered
+				)
+			)
+		);
+	}
+
+	RegisterUIActionBinding(
+		FBindUIActionArgs(
+			ICommonInputModule::GetSettings().GetDefaultBackAction(),
+			true,
+			FSimpleDelegate::CreateUObject(
+				this,
+				&ThisClass::OnBackActionTriggered
+			)
+		)
+	);
+}
+
+void UWidget_OptionScreen::OnResetActionTriggered()
+{
+}
+
+void UWidget_OptionScreen::OnBackActionTriggered()
+{
+	DeactivateWidget();
+}
--- Plugins/GameUI/Source/GameUI/Public/Widget/Options/Widget_OptionScreen.h
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Options/Widget_OptionScreen.h
@@ -14,6 +14,20 @@ class GAMEUI_API UWidget_OptionScreen : public UWidget_ActivatableBase
 {
 	GENERATED_BODY()
 	
+protected:
+	// ~Begin UUserWidget interface
+	virtual void NativeOnInitialized() override;
+	// ~End UUserWidget interface
 	
+private:
+	void OnResetActionTriggered();
+	void OnBackActionTriggered();
+	
+	UPROPERTY(EditDefaultsOnly,
+		Category = "GameUI|OptionScreen", 
+		meta = (RowType = "/Script/CommonUI.CommonInputActionDataBase"))
+	FDataTableRowHandle ResetAction;
+	
+	FUIActionBindingHandle ResetAction_Handle;
 	
 };
