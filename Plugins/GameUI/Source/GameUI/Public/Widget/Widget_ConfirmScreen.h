2af19b083e626a583819498a352a3b402cd3bddc
Z1145IKUN <2065968420@qq.com>
2025-12-19 12:41:54 +0800

    Add UWidget_ConfirmScreen file

--- Plugins/GameUI/Source/GameUI/Public/Widget/Widget_ConfirmScreen.h
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Widget_ConfirmScreen.h
@@ -4,14 +4,64 @@
 
 #include "CoreMinimal.h"
 #include "Widget/Widget_ActivatableBase.h"
+#include "GameUITypes/GameUIEnums.h"
 #include "Widget_ConfirmScreen.generated.h"
+
+class UCommonTextBlock;
+class UDynamicEntryBox;
+
+USTRUCT(BlueprintType)
+struct FConfirmScreenButtonInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EConfirmScreenButtonType ConfirmScreenButtonType = EConfirmScreenButtonType::Unknown;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText ButtonTextToDisplay;
+};
+
+UCLASS()
+class GAMEUI_API UConfirmScreenInfoObject : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	static UConfirmScreenInfoObject* CreateOkScreen(const FText& InScreenTitle, const FText& InScreenMessage);
+	static UConfirmScreenInfoObject* CreateYesNoScreen(const FText& InScreenTitle, const FText& InScreenMessage);
+	static UConfirmScreenInfoObject* CreateOkCancelScreen(const FText& InScreenTitle, const FText& InScreenMessage);
+
+	UPROPERTY(Transient)
+	FText ConfirmScreenTitle;
+
+	UPROPERTY(Transient)
+	FText ConfirmScreenMessage;
+
+	UPROPERTY(Transient)
+	TArray<FConfirmScreenButtonInfo> AvailableScreenButtons;
+};
 
 /**
  * 
  */
-UCLASS(Abstract,BlueprintType,meta=(DisableNativeTick))
+UCLASS(Abstract, BlueprintType, meta=(DisableNativeTick))
 class GAMEUI_API UWidget_ConfirmScreen : public UWidget_ActivatableBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	void InitConfirmScreen(
+		UConfirmScreenInfoObject* InScreenInfoObject,
+		TFunction<void(EConfirmScreenButtonType)> ClickedButtonCallBack);
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UCommonTextBlock> CommonTextBlock_Title;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UCommonTextBlock> CommonTextBlock_Message;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UDynamicEntryBox> DynamicEntryBox_Buttons;
 };
