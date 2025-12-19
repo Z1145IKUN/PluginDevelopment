70be7fda3c53e22fc13df098d962edc4a29a009d
Z1145IKUN <2065968420@qq.com>
2025-12-19 22:47:54 +0800

    create InitOptionDataRegistry function in it,init all tab collection

--- Plugins/GameUI/Source/GameUI/Private/Widget/Options/OptionDataRegistry.cpp
+++ Plugins/GameUI/Source/GameUI/Private/Widget/Options/OptionDataRegistry.cpp
@@ -2,3 +2,58 @@
 
 
 #include "Widget/Options/OptionDataRegistry.h"
+
+#include "Widget/Options/DataObject/ListDataObject_Collection.h"
+
+void UOptionDataRegistry::InitOptionDataRegistry(ULocalPlayer* InOwningLocalPlayer)
+{
+	InitGamePlayCollectionTab();
+	InitAudioCollectionTab();
+	InitVideoCollectionTab();
+	InitControlCollectionTab();
+}
+
+TArray<UListDataObject_Collection*> UOptionDataRegistry::GetRegisteredOptionTabCollections() const
+{
+	return RegisteredOptionsTabCollections;
+}
+
+void UOptionDataRegistry::InitGamePlayCollectionTab()
+{
+	UListDataObject_Collection* GameplayTabCollection = NewObject<UListDataObject_Collection>();
+
+	GameplayTabCollection->SetDataID(FName("GameplayTabCollection"));
+	GameplayTabCollection->SetDataDisplayName(FText::FromString("Gameplay"));
+
+	RegisteredOptionsTabCollections.Add(GameplayTabCollection);
+}
+
+void UOptionDataRegistry::InitAudioCollectionTab()
+{
+	UListDataObject_Collection* AudioTabCollection = NewObject<UListDataObject_Collection>();
+
+	AudioTabCollection->SetDataID(FName("AudioTabCollection"));
+	AudioTabCollection->SetDataDisplayName(FText::FromString("Audio"));
+
+	RegisteredOptionsTabCollections.Add(AudioTabCollection);
+}
+
+void UOptionDataRegistry::InitVideoCollectionTab()
+{
+	UListDataObject_Collection* VideoTabCollection = NewObject<UListDataObject_Collection>();
+
+	VideoTabCollection->SetDataID(FName("VideoTabCollection"));
+	VideoTabCollection->SetDataDisplayName(FText::FromString("Video"));
+
+	RegisteredOptionsTabCollections.Add(VideoTabCollection);
+}
+
+void UOptionDataRegistry::InitControlCollectionTab()
+{
+	UListDataObject_Collection* ControlTabCollection = NewObject<UListDataObject_Collection>();
+
+	ControlTabCollection->SetDataID(FName("ControlTabCollection"));
+	ControlTabCollection->SetDataDisplayName(FText::FromString("Control"));
+
+	RegisteredOptionsTabCollections.Add(ControlTabCollection);
+}
--- Plugins/GameUI/Source/GameUI/Public/Widget/Options/OptionDataRegistry.h
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Options/OptionDataRegistry.h
@@ -6,6 +6,7 @@
 #include "UObject/Object.h"
 #include "OptionDataRegistry.generated.h"
 
+class UListDataObject_Collection;
 /**
  * 
  */
@@ -16,8 +17,20 @@ class GAMEUI_API UOptionDataRegistry : public UObject
 
 public:
 	/**
-	 * 
+	 * Init all Option screen tab 
+	 * Call immediately after the option Screen is created.
 	 * @param InOwningLocalPlayer 
 	 */
 	void InitOptionDataRegistry(ULocalPlayer* InOwningLocalPlayer);
+
+	TArray<UListDataObject_Collection*> GetRegisteredOptionTabCollections() const;
+
+private:
+	void InitGamePlayCollectionTab();
+	void InitAudioCollectionTab();
+	void InitVideoCollectionTab();
+	void InitControlCollectionTab();
+
+	UPROPERTY(Transient)
+	TArray<UListDataObject_Collection*> RegisteredOptionsTabCollections;
 };
