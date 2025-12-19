f06b9a3895313836c3d2cb5fa812052f1284c2eb
Z1145IKUN <2065968420@qq.com>
2025-12-19 15:36:04 +0800

    Create Widget_OptionScreen file

---
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Widget_OptionScreen.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widget/Widget_ActivatableBase.h"
+#include "Widget_OptionScreen.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEUI_API UWidget_OptionScreen : public UWidget_ActivatableBase
+{
+	GENERATED_BODY()
+	
+};
