eed6bcacf9c84797c17765b0b326afc3070cbd24
Z1145IKUN <2065968420@qq.com>
2025-12-19 19:19:52 +0800

    add OptionDataRegistry file

---
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Options/OptionDataRegistry.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "OptionDataRegistry.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEUI_API UOptionDataRegistry : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * 
+	 * @param InOwningLocalPlayer 
+	 */
+	void InitOptionDataRegistry(ULocalPlayer* InOwningLocalPlayer);
+};
