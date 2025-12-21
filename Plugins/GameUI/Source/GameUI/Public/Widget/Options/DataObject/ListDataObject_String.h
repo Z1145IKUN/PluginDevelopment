5a0bc9dd04722bdddb1d20d9f3fd0ca5b2d1683c
Z1145IKUN <2065968420@qq.com>
2025-12-21 12:52:56 +0800

    Feat : ListDataObject - Add ListDataObject_String

---
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Options/DataObject/ListDataObject_String.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ListDataObject_Value.h"
+#include "ListDataObject_String.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEUI_API UListDataObject_String : public UListDataObject_Value
+{
+	GENERATED_BODY()
+};
