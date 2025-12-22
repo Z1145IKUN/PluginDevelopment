c2841551aed736c6d3efc403fa757cc3689a12ac
Z1145IKUN <2065968420@qq.com>
2025-12-22 22:13:05 +0800

    Feat: GameUIGameUserSettings -add GameUIGameUserSettings file

---
+++ Plugins/GameUI/Source/GameUI/Public/DeveloperSettings/GameUIGameUserSettings.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "GameUIGameUserSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEUI_API UGameUIGameUserSettings : public UGameUserSettings
+{
+	GENERATED_BODY()
+};
