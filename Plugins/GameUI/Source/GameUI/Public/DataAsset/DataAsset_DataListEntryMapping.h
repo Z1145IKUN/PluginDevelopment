99e76fff50df79ea36e46049ca5ad7bc66067c5c
Z1145IKUN <2065968420@qq.com>
2025-12-22 11:52:35 +0800

    Feat: DataAsset_DataListEntryMapping - Add ListDataListEntryMap - Add GetListEntryByListData function

--- Plugins/GameUI/Source/GameUI/Public/DataAsset/DataAsset_DataListEntryMapping.h
+++ Plugins/GameUI/Source/GameUI/Public/DataAsset/DataAsset_DataListEntryMapping.h
@@ -6,6 +6,8 @@
 #include "Engine/DataAsset.h"
 #include "DataAsset_DataListEntryMapping.generated.h"
 
+class UListDataObject_Base;
+class UWidget_ListEntry_Base;
 /**
  * 
  */
@@ -13,4 +15,16 @@ UCLASS()
 class GAMEUI_API UDataAsset_DataListEntryMapping : public UDataAsset
 {
 	GENERATED_BODY()
+
+public:
+	/**
+	 * Obtain the actual class of the data object through GetClass(), and then traverse up the inheritance chain
+	 * @param InListData An instance pointer of a list data object
+	 * @return 
+	 */
+	TSubclassOf<UWidget_ListEntry_Base> GetListEntryByListData(UListDataObject_Base* InListData);
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	TMap<TSubclassOf<UListDataObject_Base>, TSubclassOf<UWidget_ListEntry_Base>> ListDataListEntryMap;
 };
