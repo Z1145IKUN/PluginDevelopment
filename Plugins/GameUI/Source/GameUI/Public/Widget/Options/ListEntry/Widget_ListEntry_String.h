51fe77e42bcc9ff10e0cf016b6c88f2c5f6d8571
Z1145IKUN <2065968420@qq.com>
2025-12-22 13:39:41 +0800

    Feat: ListEntry_String -add some bind widget

--- Plugins/GameUI/Source/GameUI/Public/Widget/Options/ListEntry/Widget_ListEntry_String.h
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Options/ListEntry/Widget_ListEntry_String.h
@@ -6,11 +6,25 @@
 #include "Widget/Options/ListEntry/Widget_ListEntry_Base.h"
 #include "Widget_ListEntry_String.generated.h"
 
+class UGameUIButtonBase;
+class UGameUIRotator;
 /**
  * 
  */
-UCLASS()
+UCLASS(Abstract, BlueprintType, meta=(DisableNativeTick))
 class GAMEUI_API UWidget_ListEntry_String : public UWidget_ListEntry_Base
 {
 	GENERATED_BODY()
+
+private:
+	//***** Bound Widget *****//
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget, AllowPrivateAccess = "true"))
+	TObjectPtr<UGameUIButtonBase> CommonButton_Previous;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget, AllowPrivateAccess = "true"))
+	TObjectPtr<UGameUIRotator> CommonRotator_Options;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget, AllowPrivateAccess = "true"))
+	TObjectPtr<UGameUIButtonBase> CommonButton_Next;
+	//***** Bound Widget *****//
 };
