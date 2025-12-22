1c437b1cd2c4dbe87ff5fb17b891d4a0bae7cea0
Z1145IKUN <2065968420@qq.com>
2025-12-22 12:09:55 +0800

    Feat: UGameUIListView - Add UDataAsset_DataListEntryMapping - Add ValidateCompiledDefaults

--- Plugins/GameUI/Source/GameUI/Private/Widget/Component/GameUIListView.cpp
+++ Plugins/GameUI/Source/GameUI/Private/Widget/Component/GameUIListView.cpp
@@ -2,3 +2,21 @@
 
 
 #include "Widget/Component/GameUIListView.h"
+
+#include "Editor/WidgetCompilerLog.h"
+
+#if WITH_EDITOR
+void UGameUIListView::ValidateCompiledDefaults(class IWidgetCompilerLog& CompileLog) const
+{
+	Super::ValidateCompiledDefaults(CompileLog);
+
+	if (!DataAsset_DataListEntryMapping)
+	{
+		CompileLog.Error(
+			FText::FromString(TEXT("the variable DataAsset_DataListEntryMapping has not valid data asset assign")
+				+ GetClass()->GetName()
+				+ TEXT("need a valid data asset to function properly"))
+		);
+	}
+}
+#endif
--- Plugins/GameUI/Source/GameUI/Public/Widget/Component/GameUIListView.h
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Component/GameUIListView.h
@@ -6,6 +6,7 @@
 #include "CommonListView.h"
 #include "GameUIListView.generated.h"
 
+class UDataAsset_DataListEntryMapping;
 /**
  * 
  */
@@ -13,4 +14,12 @@ UCLASS()
 class GAMEUI_API UGameUIListView : public UCommonListView
 {
 	GENERATED_BODY()
+
+private:
+#if WITH_EDITOR
+	virtual void ValidateCompiledDefaults(class IWidgetCompilerLog& CompileLog) const override;
+#endif
+
+	UPROPERTY(EditAnywhere, Category="GameUI|ListView")
+	TObjectPtr<UDataAsset_DataListEntryMapping> DataAsset_DataListEntryMapping;
 };
