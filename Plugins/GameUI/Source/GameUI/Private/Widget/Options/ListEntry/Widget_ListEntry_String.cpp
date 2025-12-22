f1f8447e025183afb280a7fa7e595a8e44a6d778
Z1145IKUN <2065968420@qq.com>
2025-12-22 13:28:50 +0800

    Feat: ListEntry_String - add ListEntry_String file

---
+++ Plugins/GameUI/Source/GameUI/Private/Widget/Options/ListEntry/Widget_ListEntry_String.cpp
@@ -0,0 +1,4 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widget/Options/ListEntry/Widget_ListEntry_String.h"
---
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Options/ListEntry/Widget_ListEntry_String.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widget/Options/ListEntry/Widget_ListEntry_Base.h"
+#include "Widget_ListEntry_String.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEUI_API UWidget_ListEntry_String : public UWidget_ListEntry_Base
+{
+	GENERATED_BODY()
+};
