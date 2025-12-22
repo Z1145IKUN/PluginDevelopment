22ac4696aaf0ea42a39db1dde976c9bec24382d6
Z1145IKUN <2065968420@qq.com>
2025-12-22 23:15:10 +0800

    Feat: OptionsDataInteractionHelper -add GetValueAsString and SetValueFromString function

--- Plugins/GameUI/Source/GameUI/Private/Widget/Options/OptionsDataInteractionHelper.cpp
+++ Plugins/GameUI/Source/GameUI/Private/Widget/Options/OptionsDataInteractionHelper.cpp
@@ -2,3 +2,32 @@
 
 
 #include "Widget/Options/OptionsDataInteractionHelper.h"
+
+#include "DeveloperSettings/GameUIGameUserSettings.h"
+
+FOptionsDataInteractionHelper::FOptionsDataInteractionHelper(const FString& InGetterOrSetterFuncPath)
+{
+	CachedDynamicFunctionPath = InGetterOrSetterFuncPath;
+	CachedWeakGameUserSettings = UGameUIGameUserSettings::Get();
+}
+
+FString FOptionsDataInteractionHelper::GetValueAsString()
+{
+	FString OutStringValue;
+	PropertyPathHelpers::GetPropertyValueAsString(
+		CachedWeakGameUserSettings.Get(),
+		CachedDynamicFunctionPath,
+		OutStringValue
+	);
+
+	return OutStringValue;
+}
+
+void FOptionsDataInteractionHelper::SetValueFromString(const FString& InStringValue)
+{
+	PropertyPathHelpers::SetPropertyValueFromString(
+		CachedWeakGameUserSettings.Get(),
+		CachedDynamicFunctionPath,
+		InStringValue
+	);
+}
--- Plugins/GameUI/Source/GameUI/Public/Widget/Options/OptionsDataInteractionHelper.h
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Options/OptionsDataInteractionHelper.h
@@ -3,10 +3,21 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "PropertyPathHelpers.h"
 
+class UGameUIGameUserSettings;
 /**
  * 
  */
-class GAMEUI_API OptionsDataInteractionHelper
+class GAMEUI_API FOptionsDataInteractionHelper
 {
+public:
+	FOptionsDataInteractionHelper(const FString& InGetterOrSetterFuncPath);
+
+	FString GetValueAsString();
+	void SetValueFromString(const FString& InStringValue);
+
+private:
+	FCachedPropertyPath CachedDynamicFunctionPath;
+	TWeakObjectPtr<UGameUIGameUserSettings> CachedWeakGameUserSettings;
 };
