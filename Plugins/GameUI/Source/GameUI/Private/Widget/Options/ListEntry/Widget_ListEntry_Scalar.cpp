6b943843f7242bc3c1a9d2791b5a232b9c50297f
Z1145IKUN <2065968420@qq.com>
2025-12-25 19:27:11 +0800

    Feat: ListEntry_Scalar -add ListEntry_Scalar file

---
+++ Plugins/GameUI/Source/GameUI/Private/Widget/Options/ListEntry/Widget_ListEntry_Scalar.cpp
@@ -0,0 +1,4 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widget/Options/ListEntry/Widget_ListEntry_Scalar.h"
---
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Options/ListEntry/Widget_ListEntry_Scalar.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widget_ListEntry_Base.h"
+#include "Widget_ListEntry_Scalar.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, meta=(DisableNativeTick))
+class GAMEUI_API UWidget_ListEntry_Scalar : public UWidget_ListEntry_Base
+{
+	GENERATED_BODY()
+};
