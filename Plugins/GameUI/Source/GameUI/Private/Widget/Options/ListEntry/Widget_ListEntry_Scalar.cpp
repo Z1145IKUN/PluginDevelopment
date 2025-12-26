fe2ee74d1f01971925336a53706a9bd45a2819c1
Z1145IKUN <2065968420@qq.com>
2025-12-26 11:13:01 +0800

    Feat: ListEntry_Scalar -Add the bindWidget function and rewrite some functions of the parent class

--- Plugins/GameUI/Source/GameUI/Private/Widget/Options/ListEntry/Widget_ListEntry_Scalar.cpp
+++ Plugins/GameUI/Source/GameUI/Private/Widget/Options/ListEntry/Widget_ListEntry_Scalar.cpp
@@ -2,3 +2,18 @@
 
 
 #include "Widget/Options/ListEntry/Widget_ListEntry_Scalar.h"
+
+void UWidget_ListEntry_Scalar::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+}
+
+void UWidget_ListEntry_Scalar::OnListDataObjectSet(UListDataObject_Base* InListDataObject)
+{
+	Super::OnListDataObjectSet(InListDataObject);
+}
+
+void UWidget_ListEntry_Scalar::OnListDataObjectModified(UListDataObject_Base* ModifiedData,
+                                                        EOptionsListDataModifyReason ModifyReason)
+{
+}
--- Plugins/GameUI/Source/GameUI/Public/Widget/Options/ListEntry/Widget_ListEntry_Scalar.h
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Options/ListEntry/Widget_ListEntry_Scalar.h
@@ -6,6 +6,9 @@
 #include "Widget_ListEntry_Base.h"
 #include "Widget_ListEntry_Scalar.generated.h"
 
+
+class UCommonNumericTextBlock;
+class UAnalogSlider;
 /**
  * 
  */
@@ -13,4 +16,24 @@ UCLASS(Abstract, BlueprintType, meta=(DisableNativeTick))
 class GAMEUI_API UWidget_ListEntry_Scalar : public UWidget_ListEntry_Base
 {
 	GENERATED_BODY()
+
+protected:
+	//~ Begin UUserWidget interface
+	virtual void NativeOnInitialized() override;
+	//~ End UUserWidget interface
+
+	//~ Begin UWidget_ListEntry_Base interface
+	virtual void OnListDataObjectSet(UListDataObject_Base* InListDataObject) override;
+	virtual void OnListDataObjectModified(UListDataObject_Base* ModifiedData,
+	                                      EOptionsListDataModifyReason ModifyReason) override;
+	//~ Begin UWidget_ListEntry_Base interface
+
+private:
+	//***** Bound Widget *****//
+	UPROPERTY(BlueprintReadOnly, meta=(AllowPrivateAccess="true", BindWidget))
+	TObjectPtr<UCommonNumericTextBlock> CommonNumericTextBlock_SettingValue;
+
+	UPROPERTY(BlueprintReadOnly, meta=(AllowPrivateAccess="true", BindWidget))
+	TObjectPtr<UAnalogSlider> AnalogSlider_SettingSlider;
+	//***** Bound Widget *****//
 };
