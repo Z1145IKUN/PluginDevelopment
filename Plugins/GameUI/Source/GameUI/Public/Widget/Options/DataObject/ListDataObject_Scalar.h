5890ecb3c1f9cf6a2cc0879e981edccf67d391c4
Z1145IKUN <2065968420@qq.com>
2025-12-26 11:40:59 +0800

    Feat: ListDataObject_Scalar -Add ListDataObject_Scalar file

---
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Options/DataObject/ListDataObject_Scalar.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ListDataObject_Value.h"
+#include "ListDataObject_Scalar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEUI_API UListDataObject_Scalar : public UListDataObject_Value
+{
+	GENERATED_BODY()
+};
