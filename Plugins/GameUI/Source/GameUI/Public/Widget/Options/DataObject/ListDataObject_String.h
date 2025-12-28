4bd8594726797b6f87dcdea9e200b157febe7820
Z1145IKUN <2065968420@qq.com>
2025-12-28 17:29:54 +0800

    Feat: ListDataObject_String -add UListDataObject_StringEnum in UListDataObject_String

--- Plugins/GameUI/Source/GameUI/Public/Widget/Options/DataObject/ListDataObject_String.h
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Options/DataObject/ListDataObject_String.h
@@ -65,3 +65,35 @@ private:
 	const FString TrueString = TEXT("true");
 	const FString FalseString = TEXT("false");
 };
+
+UCLASS()
+class GAMEUI_API UListDataObject_StringEnum : public UListDataObject_String
+{
+	GENERATED_BODY()
+
+public:
+	template <typename EnumType>
+	void AddEnumOption(EnumType InEnumOption, const FText& InDisplayText)
+	{
+		const UEnum* StaticEnumOption = StaticEnum<EnumType>();
+		const FString ConvertedEnumString = StaticEnumOption->GetNameStringByValue(InEnumOption);
+
+		AddDynamicOptions(ConvertedEnumString, InDisplayText);
+	}
+
+	template <typename EnumType>
+	EnumType GetCurrentValueAsEnum() const
+	{
+		const UEnum* StaticEnumOption = StaticEnum<EnumType>();
+		return static_cast<EnumType>(StaticEnumOption->GetValueByNameString(CurrentStringValue));
+	}
+
+	template <typename EnumType>
+	void SetDefaultValueFromEnumOption(EnumType InEnumOption)
+	{
+		const UEnum* StaticEnumOption = StaticEnum<EnumType>();
+		const FString ConvertedEnumString = StaticEnumOption->GetNameStringByValue(InEnumOption);
+
+		SetDefaultValueFromString(ConvertedEnumString);
+	}
+};
