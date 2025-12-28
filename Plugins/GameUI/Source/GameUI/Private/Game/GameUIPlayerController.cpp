deb438a1ce9db223f47070fc9d5d13a1709cb2e4
Z1145IKUN <2065968420@qq.com>
2025-12-28 17:29:19 +0800

    Feat:GameUIPlayerController add RunHardwareBenchmark in PlayerController

--- Plugins/GameUI/Source/GameUI/Private/Game/GameUIPlayerController.cpp
+++ Plugins/GameUI/Source/GameUI/Private/Game/GameUIPlayerController.cpp
@@ -4,6 +4,7 @@
 #include "Game/GameUIPlayerController.h"
 
 #include "Camera/CameraActor.h"
+#include "DeveloperSettings/GameUIGameUserSettings.h"
 #include "Kismet/GameplayStatics.h"
 
 void AGameUIPlayerController::OnPossess(APawn* InPawn)
@@ -11,10 +12,22 @@ void AGameUIPlayerController::OnPossess(APawn* InPawn)
 	Super::OnPossess(InPawn);
 
 	TArray<AActor*> FoundCameras;
-	UGameplayStatics::GetAllActorsOfClassWithTag(this, ACameraActor::StaticClass(), CameraTag, FoundCameras);
+	UGameplayStatics::GetAllActorsOfClassWithTag(
+		this,
+		ACameraActor::StaticClass(),
+		CameraTag,
+		FoundCameras);
 
 	if (!FoundCameras.IsEmpty())
 	{
 		SetViewTarget(FoundCameras[0]);
+	}
+
+	UGameUIGameUserSettings* GameUserSettings = UGameUIGameUserSettings::Get();
+
+	if (GameUserSettings->GetLastCPUBenchmarkResult() == -1.f || GameUserSettings->GetLastGPUBenchmarkResult() == -1.f)
+	{
+		GameUserSettings->RunHardwareBenchmark();
+		GameUserSettings->ApplyHardwareBenchmarkResults();
 	}
 }
