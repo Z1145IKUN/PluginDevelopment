5841c6934fe7e88144d7460dfeb8f681ae1a5fa4
Z1145IKUN <2065968420@qq.com>
2025-12-28 19:59:09 +0800

    Feat: -add ListDataObject_StringResolution

---
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Options/DataObject/ListDataObject_StringResolution.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widget/Options/DataObject/ListDataObject_String.h"
+#include "ListDataObject_StringResolution.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEUI_API UListDataObject_StringResolution : public UListDataObject_String
+{
+	GENERATED_BODY()
+};
