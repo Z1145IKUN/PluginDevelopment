cb0bea796ac333a3e3f32148ed0d61fa7b6c858c
Z1145IKUN <2065968420@qq.com>
2025-12-29 19:54:06 +0800

    Feat: GameUIStructs -add FOptionsDataEditConditionDescriptor

---
+++ Plugins/GameUI/Source/GameUI/Public/GameUITypes/GameUIStructs.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+
+#include "GameUIStructs.generated.h"
+
+USTRUCT()
+struct FOptionsDataEditConditionDescriptor
+{
+	GENERATED_BODY()
+
+public:
+	void SetEditConditionFunc(const TFunction<bool()>& InEditConditionFunc)
+	{
+		EditConditionFunc = InEditConditionFunc;
+	}
+
+	bool IsValid() const
+	{
+		return EditConditionFunc != nullptr;
+	}
+
+	bool IsEditConditionMet() const
+	{
+		if (IsValid())
+		{
+			return EditConditionFunc();
+		}
+		return true;
+	}
+
+	FString GetDisabledRichReason() const
+	{
+		return DisabledRichReason;
+	}
+
+	void SetDisabledRichReason(const FString& InDisabledRichReason)
+	{
+		DisabledRichReason = InDisabledRichReason;
+	}
+
+	bool HasForceStringValue() const
+	{
+		return DisabledForcedStringValue.IsSet();
+	}
+
+	FString GetDisabledForcedStringValue() const
+	{
+		return DisabledForcedStringValue.GetValue();
+	}
+
+	void SetDisabledForcedStringValue(const FString& InDisabledForcedStringValue)
+	{
+		DisabledForcedStringValue = InDisabledForcedStringValue;
+	}
+
+private:
+	TFunction<bool()> EditConditionFunc;
+	FString DisabledRichReason;
+	TOptional<FString> DisabledForcedStringValue;
+};
