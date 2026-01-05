6bc184c9aaaa58122f93d4e3acf51ecd836b1559
Z1145IKUN <2065968420@qq.com>
2026-01-05 23:54:03 +0800

    Feat: Widget_ListEntry_KeyRemap -add empty class Widget_ListEntry_KeyRemap

---
+++ Plugins/GameUI/Source/GameUI/Public/Widget/Options/ListEntry/Widget_ListEntry_KeyRemap.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widget_ListEntry_Base.h"
+#include "Widget_ListEntry_KeyRemap.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEUI_API UWidget_ListEntry_KeyRemap : public UWidget_ListEntry_Base
+{
+	GENERATED_BODY()
+};
