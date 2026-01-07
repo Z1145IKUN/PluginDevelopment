fbe08a0560caaa115667eee0f6dfa6204962cd1a
Z1145IKUN <2065968420@qq.com>
2026-01-07 19:14:25 +0800

    Feat: -add load screen widget and Load screen settings,

---
+++ Plugins/GameUI/Source/GameUI/Private/DeveloperSettings/GameUILoadScreenSetting.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DeveloperSettings/GameUILoadScreenSetting.h"
+#include "Blueprint/UserWidget.h"
+
+TSubclassOf<UUserWidget> UGameUILoadScreenSetting::GetLoadScreenWidgetClassChecked() const
+{
+	check(!SoftLoadScreenWidgetClass.IsNull())
+
+	TSubclassOf<UUserWidget> LoadedLoadingScreenWidget = SoftLoadScreenWidgetClass.LoadSynchronous();
+
+	return LoadedLoadingScreenWidget;
+}
---
+++ Plugins/GameUI/Source/GameUI/Public/DeveloperSettings/GameUILoadScreenSetting.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "GameUILoadScreenSetting.generated.h"
+
+class UUserWidget;
+/**
+ * 
+ */
+UCLASS(Config=Game, DefaultConfig)
+class GAMEUI_API UGameUILoadScreenSetting : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	TSubclassOf<UUserWidget> GetLoadScreenWidgetClassChecked() const;
+
+	UPROPERTY(Config, EditAnywhere, Category="Load Screen Settings")
+	TSoftClassPtr<UUserWidget> SoftLoadScreenWidgetClass;
+
+	UPROPERTY(Config, EditAnywhere, Category="Load Screen Settings")
+	float HoldLoadScreenExtraSeconds = 3.f;
+
+	UPROPERTY(Config, EditAnywhere, Category="Load Screen Settings")
+	bool bShouldLoadScreenInEditor = false;
+};
