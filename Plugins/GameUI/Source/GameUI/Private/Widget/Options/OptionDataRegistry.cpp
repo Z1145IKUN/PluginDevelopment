0ac0da6fdce3c0ea66e7785ab84fd917fb2fe7fc
Z1145IKUN <2065968420@qq.com>
2026-01-18 15:27:42 +0800

    Feat: -Add fonts to make the project compatible with Chinese

--- Plugins/GameUI/Source/GameUI/Private/Widget/Options/OptionDataRegistry.cpp
+++ Plugins/GameUI/Source/GameUI/Private/Widget/Options/OptionDataRegistry.cpp
@@ -99,7 +99,7 @@ void UOptionDataRegistry::InitGamePlayCollectionTab()
 			LanguageOption->SetDataID(FName("LanguageOption"));
 			LanguageOption->SetDataDisplayName(LOCTEXT("Language", "Language"));
 			LanguageOption->AddDynamicOptions(TEXT("en"), LOCTEXT("Language_English", "English"));
-			LanguageOption->AddDynamicOptions(TEXT("zh"), LOCTEXT("Language_Chinese", "Chinese"));
+			LanguageOption->AddDynamicOptions(TEXT("zh-Hans-CN"), LOCTEXT("Language_Chinese", "Chinese"));
 			LanguageOption->SetDefaultValueFromString(TEXT("en"));
 			LanguageOption->SetDataDynamicGetter(MAKE_DATA_OPTION_CONTROL(GetCurrentLanguage));
 			LanguageOption->SetDataDynamicSetter(MAKE_DATA_OPTION_CONTROL(SetCurrentLanguage));
